e0cfb0a171b1a49caa6cd24f285d9ebbb00fab14
phisham <84333011+phisham@users.noreply.github.com>
2021-07-27 00:02:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,32 +11,13 @@ app.get("/",function(req,res){
     res.sendFile(__dirname+"/IND.html");
 })
 app.post("/",function(req,res){
-    
-    var mes=req.body.n3;
-
-    var transporter=nodemailer.createTransport({
-        service:'gmail',
-        auth: {
-            user:'nazeefaayisha@gmail.com',
-            pass:'123OneTwo',
-
-
-        }
-    });
-    var mailOptions= {
-        from:'nazeefaayisha@gmail.com',
-        to:'phisham100@gmail.com',
-        subject:'contact form',
-        text:mes
-    };
-    transporter.sendMail(mailOptions,function(err){
-        if (error) {
-            console.log(error);
-        } else {
-            console.log('Email sent: ' + info.response);
-        }
-    });
-
+    var n=req.body.n3;
+    var filename=fs.appendFile('msg.txt',n+'',function(err){
+        if(err) throw err;
+        
+    })
+    res.send('submitted successfully');
+   
 })
 app.listen(PORT,function(req,res){
     console.log("Server has started on port");
