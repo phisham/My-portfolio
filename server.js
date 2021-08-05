6d81db1a17960355d27b9ae7c4f15f99c4376d8d
phisham <84333011+phisham@users.noreply.github.com>
2021-08-05 13:15:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,19 +3,39 @@ const bodyParser=require("body-parser");
 const { ifError } = require("assert");
 const app=express();
 const fs=require('fs');
-var PORT=process.env.PORT || 5000;
 var nodemailer=require('nodemailer');
+const { info } = require("console");
+var PORT=process.env.PORT || 5000;
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/",function(req,res){
     res.sendFile(__dirname+"/IND.html");
 })
+
 app.post("/",function(req,res){
-    var n=req.body.n3;
-    var filename=fs.appendFile('msg.txt',n+'',function(err){
-        if(err) throw err;
+   var n=req.body.n3;
+   var transporter=nodemailer.createTransport({
+       service:'gmail',
+       auth: {
+           user: 'Nodemailer101@gmail.com',
+           pass: 'Node@success'
         
-    })
+       }
+   });
+   var mailOptions= {
+       from:'Nodemailer101@gmail.com',
+       to:'phisham100@gmail.com',
+       subject: 'NODEJS EMAIL',
+       text: n
+
+   };
+   transporter.sendMail(mailOptions,function(error,info){
+       if(error){
+           console.log(error);
+       } else {
+           console.log('Email sent'+info.response);
+       }
+   });
    
    
 })
